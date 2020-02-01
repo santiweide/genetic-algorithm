89aaec7b16f17cc4651b186293b202ad331f968e
santiweide <ggy_778@mail.dlut.edu.cn>
2020-02-01 21:09:45 +0800

    guess a random string with benchmark

---
+++ genetic.py
@@ -0,0 +1,73 @@
+import random
+import time
+import statistics
+import sys
+
+
+class Chromosome:
+    Genes = None
+    Fitness = None
+
+    def __init__(self, genes, fitness):
+        self.Genes = genes
+        self.Fitness = fitness
+
+
+class Benchmark:
+    @staticmethod
+    def run(function):
+        timings = []
+        stdout = sys.stdout
+        for i in range(100):
+            sys.stdout = None
+            start_time = time.time()
+            function()
+            seconds = time.time() - start_time
+            sys.stdout = stdout
+            timings.append(seconds)
+            mean = statistics.mean(timings)
+            if i < 10 or i % 10 == 9:
+                print("{0} {1:3.2f} {2:3.2f}".format(
+                    1+i, mean,
+                    statistics.stdev(timings, mean)
+                    if i > 1 else 0))
+
+
+def _generate_parent(length, gene_set,get_fitness):
+    genes = []
+    while len(genes) < length:
+        # 一位一位随机也可以，但是长一点比较可以体现不重复随机的优势？其实用带重复随机的也可以。
+        sample_size = min(length - len(genes), len(gene_set))
+        genes.extend(random.sample(gene_set, sample_size))
+    genes = ''.join(genes)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
+
+
+def _mutate(parent, geneSet, get_fitness):
+    index = random.randrange(0, len(parent.Genes))
+    child_genes = list(parent.Genes)
+    new_gene, alternate = random.sample(geneSet, 2)
+    if new_gene == child_genes[index]:
+        child_genes[index] = alternate
+    else:
+        child_genes[index] = new_gene
+    genes = ''.join(child_genes)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
+
+
+def get_best(get_fitness, target_len, optimal_fitness, gene_set, display):
+    random.seed()
+    best_parent = _generate_parent(target_len, gene_set, get_fitness)
+    display(best_parent)
+    if best_parent.Fitness >= optimal_fitness:
+        return best_parent
+    while True:
+        child = _mutate(best_parent, gene_set, get_fitness)
+        if best_parent.Fitness >= child.Fitness:
+            continue
+        display(child)
+        if child.Fitness >= optimal_fitness:
+            return child
+        best_parent = child
---
+++ guessPasswordTests.py
@@ -0,0 +1,42 @@
+import datetime
+import genetic
+import unittest
+import random
+
+def display(candidate, start_time):
+    time_diff = datetime.datetime.now() - start_time
+    print("{0}\t{1}\t{2}".format(candidate.Genes, candidate.Fitness, str(time_diff)))
+
+
+def get_fitness(genes, target):
+    return sum(1 for expected, actual in zip(target, genes)
+               if expected == actual)
+
+
+class GuessPasswordTests(unittest.TestCase):
+    gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,"
+
+    def test_random(self):
+        length = 150
+        target = ''.join(random.choice(self.gene_set) for _ in range(length))
+        self.guess_password(target)
+
+    def test_benchmark(self):
+        genetic.Benchmark.run(self.test_random)
+
+    def guess_password(self,target):
+        start_time = datetime.datetime.now()
+
+        def fnGetFitness(genes):
+            return get_fitness(genes, target)
+
+        def fnDisplay(candidate):
+            display(candidate, start_time)
+
+        optimal_fitness = len(target)
+        best = genetic.get_best(fnGetFitness, len(target), optimal_fitness, self.gene_set, fnDisplay)
+        self.assertEqual(best.Genes, target)
+
+
+if __name__ == '__main__':
+    unittest.main()
